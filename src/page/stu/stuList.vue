7e3b9cc6da8c5ef0dc90f9c59362ee2c51e2f135
e <yanxiaodong@lvya.org>
2019-03-29 16:37:14 +0800

    学生给学生填写列表添加分页加载

--- src/page/stu/stuList.vue
+++ src/page/stu/stuList.vue
@@ -7,28 +7,32 @@
                 <i class="icon iconfont icon-shiyongcishu ripple"></i>
             </span> -->
         </div>
-        <dir class="stuListPar">
-            <div class="ripple" v-for="(item,index) in conData" :key="index" @click="link(item.stuId)">
-                <img class="first-img" src="../../assets/img/yiwancheng.png" v-if="item.taskState== 4">
-                <img class="first-img" src="../../assets/img/jinxingzhong.png" v-if="item.taskState==1">
-                <img class="first-img" src="../../assets/img/daishenhe_t.png" v-if="item.taskState==2">
-                <img class="first-img" src="../../assets/img/yibohui_t.png" v-if="item.taskState==3">
-                <img class="first-img" src="../../assets/img/yijieshu.png" v-if="item.taskState==0">
-                <img class="first-img" src="../../assets/img/weikaiqi.png" v-if="item.taskState== -1">
-                <div class="ne-div">
-                    <p>
-                        <span>{{ item.stuName }}</span>
-                        <span>{{ item.orgName }}</span>
-                    </p>
-                    <p><span>{{item.updateTimeStr}}</span></p>
-                    <img class="back-a" src="../../assets/img/you.png" alt="">
+        <div class="bscroll" ref="bscroll">
+            <div class="bscroll-container">
+                <div class="stuListPar">
+                    <div class="ripple" v-for="(item,index) in conData" :key="index" @click="link(item.stuId)">
+                        <img class="first-img" src="../../assets/img/yiwancheng.png" v-if="item.taskState== 4">
+                        <img class="first-img" src="../../assets/img/jinxingzhong.png" v-if="item.taskState==1">
+                        <img class="first-img" src="../../assets/img/daishenhe_t.png" v-if="item.taskState==2">
+                        <img class="first-img" src="../../assets/img/yibohui_t.png" v-if="item.taskState==3">
+                        <img class="first-img" src="../../assets/img/yijieshu.png" v-if="item.taskState==0">
+                        <img class="first-img" src="../../assets/img/weikaiqi.png" v-if="item.taskState== -1">
+                        <div class="ne-div">
+                            <p>
+                                <span>{{ item.stuName }}</span>
+                                <span>{{ item.orgName }}</span>
+                            </p>
+                            <p><span>{{item.updateTimeStr}}</span></p>
+                            <img class="back-a" src="../../assets/img/you.png" alt="">
+                        </div>
+                    </div>
+                    <div class="no-msg-div" v-if="conData.length<=0">
+                        <img src="@/assets/img/zanwushuju.png" alt="">
+                        <span>暂无数据</span>
+                    </div>
                 </div>
             </div>
-            <div class="no-msg-div" v-if="conData.length<=0">
-                <img src="@/assets/img/zanwushuju.png" alt="">
-                <span>暂无数据</span>
-            </div>
-        </dir>
+        </div>
     </div>
 </template>
 
@@ -36,7 +40,7 @@
 import { LoadMore } from "vux";
 import qs from 'qs';
 // import {formatDate} from '@/plugins/formatDate.js';
-// import BScroll from "better-scroll";
+import BScroll from "better-scroll";
 // import showcycle from '@/page/tea/SelectionPeriod'
 
 
@@ -50,6 +54,8 @@ export default {
             formId:this.$route.params.formId,
             schoolId:this.$route.params.schoolid,
             title:'',
+            pageNo:1,
+            hasNextPage:false,//是否有下一页数据
             conData:[],
         }
     },
@@ -76,17 +82,41 @@ export default {
         },
         query(){
             console.log(this.id)
+            this.$vux.loading.show({
+                text: '加载中...'
+            });
             this.$axios.get( process.env.API_ROOT+"app/stu/v1/showStuTaskList",{
                 params:{
                     uid:this.uid,
                     taskId:Number(this.id),
                     formId:Number(this.formId),
-                    schoolId:Number(this.schoolId)
+                    schoolId:Number(this.schoolId),
+                    pageNo:this.pageNo
                 }
             }).then((res)=>{
                 if(res.success){
-                    this.conData = res.data;
-                    this.title = res.data[0].taskName;
+                    this.$vux.loading.hide();
+                    this.conData = this.conData.concat(res.data.videoStuListRespList);
+                    this.title = res.data.videoStuListRespList[0].taskName;
+                    this.hasNextPage = res.data.hasNextPage;
+                    this.$nextTick(() => {
+                        if (!this.scroll) {
+                            this.scroll = new BScroll(this.$refs.bscroll, {
+                                click: true,
+                                probeType: 2,
+                            });
+                            this.scroll.on('touchEnd', (pos) => {
+                                if(this.scroll.maxScrollY>pos.y+20){
+                                    if(!this.hasNextPage) return;
+                                    this.pageNo++;
+                                    this.query();
+                                }
+                            })
+                        }
+                        else{
+                            this.scroll.refresh();
+                        }
+                    })
                 }
             }).catch((err)=>{
                 this.errorUtil(err);
@@ -98,12 +128,24 @@ export default {
     },
 }
 </script>
-<style>
-
-</style>
 
 <style scoped>
-.top-back {text-align: center;font-size: 36px;color: #444;padding:20px;background-color: #fff;}
+.bscroll{
+    margin-top:96px;
+    height: calc(100vh - 96px);
+    overflow: hidden;
+}
+.top-back {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    padding:20px;
+    text-align: center;
+    font-size: 36px;
+    color: #444;
+    background-color: #fff;
+    z-index: 1000;}
 .top-back .img-1{position: absolute;width: 38px;height: 38px;left: 20px;top: 25px}
 .top-back .img-2{position: absolute;left: 80px;top: 15px}
 .top-back .img-2 i{color: #01c269;font-size: 40px;}
@@ -123,8 +165,4 @@ export default {
 .no-msg-div{color: #999!important;text-align: center;line-height: 64px;margin-top: 50px;text-align:center!important;}
 .no-msg-div img{width: 70px;height: 70px;display: inline-block;vertical-align: middle}
 .no-msg-div span{height: 70px;display: inline-block;vertical-align: middle;font-size:28px;}
-
-.bscroll{width: 100%;overflow: hidden;}
-.drop-down{position: absolute;top:90px;left:0px;width: 100%;height: 50px;line-height:50px;text-align: center;}
-.drop-down img{width:50px;height:50px;}
 </style>
