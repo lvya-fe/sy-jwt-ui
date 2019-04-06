5ed5cce5f262be0e3e3ac7ca84cc03b2831ef7e7
lgzhang <lgzhang08@gmail.com>
2019-04-06 22:30:32 +0800

    fix start

--- src/page/start.vue
+++ src/page/start.vue
@@ -1,43 +1,50 @@
 <template>
   <div>
-      <img src="@/assets/img/start_t.png" class="xz_t" alt="">
-      <img src="@/assets/img/start_z.png" class="xz_z" alt="">
+    <img src="@/assets/img/start_t.png" class="xz_t" alt="">
+    <img src="@/assets/img/start_z.png" class="xz_z" alt="">
   </div>
 </template>
 
 <script>
-    import Cookies from 'js-cookie'
+  import Cookies from 'js-cookie'
   import qs from 'qs';
-  import { Loading } from 'vux'
+  import {Loading} from 'vux'
+
   export default {
-    data(){
-      return{
-      }
+    data() {
+      return {}
     },
     components: {
       Loading
     },
-    created(){
+    created() {
       this.loadData();
     },
-    methods:{
-        loadData(){
-          this.$vux.loading.show({
-           text: '微信登录中...'
-          });
-          // var lasturl = localStorage.getItem("lasturl");
-          setTimeout(function() {
-              var token = Cookies.get('wxtoken');
-              var lasturl = Cookies.get('lasturl');
-              Cookies.set('wxtoken_',token);
-              location.replace("/app/auth/wx?uid="+localStorage.uid+"&code="+token+"&url="+encodeURIComponent(lasturl));
-          },1000);
+    methods: {
+      loadData() {
+        this.$vux.loading.show({
+          text: '微信登录中...'
+        });
+        // var lasturl = localStorage.getItem("lasturl");
+        var token = Cookies.get('wxtoken');
+        var lasturl = Cookies.get('lasturl');
+        Cookies.set('wxtoken_', token);
+        location.replace("/app/auth/wx?uid=" + localStorage.uid + "&code=" + token + "&url=" + encodeURIComponent(lasturl));
 
-        }
+      }
     }
   }
 </script>
 <style scoped>
-  .xz_t{width: 432px;margin: 200px auto;display: block}
-  .xz_z{width: 510px;margin: 0 auto;display: block}
+  .xz_t {
+    width: 432px;
+    margin: 200px auto;
+    display: block
+  }
+
+  .xz_z {
+    width: 510px;
+    margin: 0 auto;
+    display: block
+  }
 </style>
