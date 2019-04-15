a46a8404b8b128285614bf914497528eca35b68d
e <yanxiaodong@lvya.org>
2019-04-15 11:54:15 +0800

    学生给同学填写 单选引用样式更改

--- src/page/details/task-details.vue
+++ src/page/details/task-details.vue
@@ -62,7 +62,7 @@
                 <radioList :lists="item.formSelectItemResps" :disabled = '(![1,3].includes(formState) || item.citeDataType !=0) ? true :false' :checkVal="item.formItemValue" :index="index" @changeVal="changeRadio"></radioList>
 
               </div>
-              <FormComsItem  v-else :formItem.sync="item" :taskState="formState"></FormComsItem>
+              <FormComsItem class="paddinglr30"  v-else :formItem.sync="item" :taskState="formState"></FormComsItem>
             </div>
           </template>
 
