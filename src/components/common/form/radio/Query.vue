d03a6235bd0e676abbd9c8e88d7cc0148d2c5b90
e <yanxiaodong@lvya.org>
2019-04-15 16:17:41 +0800

    单选不引用查看样式调整

--- src/components/common/form/radio/Query.vue
+++ src/components/common/form/radio/Query.vue
@@ -8,13 +8,13 @@ event:
 -->
 
 <template>
-  <div class="form-common form-item">
+  <div class="form-common form-item readRadio">
     <div class="form-wrap field-block">
       <p class="form-name-block vux-1px-b">
         {{item.formItemName}}
       </p>
       <!--自己给自己填写，只显示-->
-      <ul class="radioLists">
+      <ul class="radioLists new">
         <li v-for="(radio,index) in item.formSelectItemResps" :class="{'vux-1px-t': index != 0}" :key="radio.id">
           <p :class="{'checked': radio.value == item.formItemValue}">{{radio.value}}</p>
         </li>
@@ -43,18 +43,22 @@ event:
   }
 </script>
 <style lang="less" scope>
-  .field-block{
-    ul{
-      padding: 0 30px;
-      li{
-        list-style: none;
-        padding: 30px 0;
-        .checked{
-          background: url('../../../../assets/img/checked.png') no-repeat right center;
-          background-size: 35px 31px;
-        }
+  .readRadio{
+    margin: 0 -30px;
+    .field-block{
+      ul{
+        padding: 0 30px;
+        li{
+          list-style: none;
+          padding: 30px 0;
+          .checked{
+            background: url('../../../../assets/img/checked.png') no-repeat right center;
+            background-size: 35px 31px;
+          }
 
+        }
       }
     }
   }
+  
 </style>
