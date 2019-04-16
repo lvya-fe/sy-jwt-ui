24e6564584e285fd4b89f11a13513d072748629a
xdf <xdf@lvya.org>
2019-04-16 16:08:01 +0800

    失误招领信息页面的优化(所有样式嵌套)

--- src/page/LF/publish/publish.vue
+++ src/page/LF/publish/publish.vue
@@ -43,49 +43,51 @@
   @border-color:#BCBCBC;
   @border-left-color:#169BD5;
   @border-color-list:#EBEBEB;
-  .claim .info{
-    height: 1.06rem;
-    line-height: 1.06rem;
-    text-align: center;
-    font-size: 0.45rem;
-    border-bottom: 1px solid @border-color;
-    margin-bottom: 0.3rem;
-    color:#444;
-  }
-  .claim .guide,.goods{
-    padding-left:0.27rem;
-    .title{
-      border-left: 0.14rem solid @border-left-color;
-      padding-left: 0.27rem;
-      height: 0.8rem;
-      line-height: 0.8rem;
-      font-size: 0.4rem;
-      color:#333333;
-    }
-    .content{
-      padding:20px;
-      text-indent: 0.86rem;
-      text-align: justify;
-      font-size:0.4rem;
-      color:@font_color;
-    }
-  }
-  .claim .goods-btn .btn-list{
-    padding:0.27rem;
-    list-style:none;
-    li{
-      display: inline-block;
-      height: 0.95rem;
-      width: 2rem;
-      line-height: 0.95rem;
-      border: 0.03rem solid @border-color-list;
+  .claim{
+    .info{
+      height: 1.06rem;
+      line-height: 1.06rem;
       text-align: center;
-      border-radius: 0.27rem;
-      cursor: pointer;
-      font-size: 0.4rem;
-      margin-right: 0.15rem;
-      margin-bottom: 0.2rem;
-      color:@font_color;
+      font-size: 0.45rem;
+      border-bottom: 1px solid @border-color;
+      margin-bottom: 0.3rem;
+      color:#444;
+    }
+    .guide,.goods{
+      padding-left:0.27rem;
+      .title{
+        border-left: 0.14rem solid @border-left-color;
+        padding-left: 0.27rem;
+        height: 0.8rem;
+        line-height: 0.8rem;
+        font-size: 0.4rem;
+        color:#333333;
+      }
+      .content{
+        padding:20px;
+        text-indent: 0.86rem;
+        text-align: justify;
+        font-size:0.4rem;
+        color:@font_color;
+      }
+    }
+    .goods-btn .btn-list{
+      padding:0.27rem;
+      list-style:none;
+      li{
+        display: inline-block;
+        height: 0.95rem;
+        width: 2rem;
+        line-height: 0.95rem;
+        border: 0.03rem solid @border-color-list;
+        text-align: center;
+        border-radius: 0.27rem;
+        cursor: pointer;
+        font-size: 0.4rem;
+        margin-right: 0.15rem;
+        margin-bottom: 0.2rem;
+        color:@font_color;
+      }
     }
   }
 </style>
