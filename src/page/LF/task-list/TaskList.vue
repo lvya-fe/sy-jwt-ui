13f9704d9d090f585db3c4b0985fdaf9f2e77e35
xdf <xdf@lvya.org>
2019-04-18 20:07:15 +0800

    修改招领信息的样式

--- src/page/LF/task-list/TaskList.vue
+++ src/page/LF/task-list/TaskList.vue
@@ -2,7 +2,7 @@
   <div class="task-list">
     <HeaderBack title="任务列表"></HeaderBack>
 
-    <div class="tab-con">
+    <div class="tab-con" id="tab-con" :class="{fixTitle:whether}">
       <tab :line-width=2 active-color='#16c775' v-model="type" class="footer-tab">
         <tab-item class="vux-center" v-for="item in typeList" :key="item.type" @on-item-click="tab">{{ item.name }}
           ({{ item.num }})
@@ -12,18 +12,20 @@
 
     <div class="search">
         <group>
-          <x-input placeholder="搜索你丢的物品" show-clear="false" class="search-input"></x-input>
+          <x-input placeholder="搜索你丢的物品" show-clear="false" class="search-input">
+          </x-input>
         </group>
+        <x-icon type="ios-search-strong" class="search-icon" size="20"></x-icon>
     </div>
-    <!--<unclaimed></unclaimed>-->
-    <claim></claim>
+    <unclaimed v-show="toogleTask==1"></unclaimed>
+    <claim v-show="toogleTask==0"></claim>
   </div>
 </template>
 
 <script>
 
 
-  import {XSwitch, Tab, TabItem, Swiper, SwiperItem, CellBox, Group, Checker, CheckerItem, LoadMore,XInput} from "vux";
+  import {XSwitch, Tab, TabItem, Group,XInput} from "vux";
   import BScroll from 'better-scroll'
   import Scroll from "@/components/common/scroll.vue";
   import Unclaimed from './unclaimed.vue'
@@ -36,8 +38,10 @@
       TabItem,
       Scroll,
       XInput,
+      Group,
       Unclaimed,
-      Claim
+      Claim,
+      BScroll
     },
     data() {
       return {
@@ -54,7 +58,8 @@
             type: 1
           }
         ],
-        value:'a'
+        value:'a',
+        toogleTask:0
       }
     },
     computed: {
@@ -66,16 +71,32 @@
     filters: {
     },
     methods: {
-      tab() {
-
+      tab(index) {
+          this.toogleTask = index;
+      },
+      throttleScroll(){
+      /*  debugger;
+        this.scroll  = document.documentElement.scrollTop || document.body.scrollTop;
+        let menuTop = document.querySelector('.task-list').offsetTop;
+        console.log(menuTop)
+        if (_self.scroll > menuTop) {
+          this.searchBarFixed = true
+        } else {
+          this.searchBarFixed = false
+        }*/
       }
     },
     mounted() {
-
+     // window.addEventListener('scroll', this.throttleScroll, false);
     },
   }
 </script>
 
 <style lang="less">
   @import "less/task-list.less";
+  .isFixed {
+    position: fixed;
+    top: 0;
+    z-index: 999;
+  }
 </style>
